5aaf626bfa1a505cf6f8fc77abe765bed41ea59a
tiffchiu <tiffxchiu@gmail.com>
2017-02-16 18:29:39 -0800

    Adds questions for user to input info about hamster

---
+++ ruby/hamsters.rb
@@ -0,0 +1,14 @@
+puts "What's the hamster's name?"
+hamster_name = gets.chomp
+
+puts "What's the hamster's volume level (from 1-10)?"
+volume = gets.chomp
+
+puts "What's the hamster's fur color?"
+fur_color = gets.chomp
+
+puts "Is the hamster a good candidate for adoption (y/n)?"
+adopt = gets.chomp
+
+puts "What is the hamster's age?"
+age = gets.chomp
