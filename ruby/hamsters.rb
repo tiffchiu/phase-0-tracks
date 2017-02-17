eca733c01da6baac111d4674bf0179920d9742fb
tiffchiu <tiffxchiu@gmail.com>
2017-02-16 18:49:19 -0800

    Converts volume and age strings to integers and floats

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,14 +1,25 @@
 puts "What's the hamster's name?"
 hamster_name = gets.chomp
+puts "#{hamster_name}"
 
 puts "What's the hamster's volume level (from 1-10)?"
-volume = gets.chomp
+volume = gets.chomp.to_i
+puts volume.class
 
 puts "What's the hamster's fur color?"
 fur_color = gets.chomp
+puts "#{fur_color}"
 
 puts "Is the hamster a good candidate for adoption (y/n)?"
 adopt = gets.chomp
+puts "#{adopt}"
 
 puts "What is the hamster's age?"
-age = gets.chomp
+age = gets.chomp.to_f
+puts age.class
+
+if age == ""
+	age = nil
+	puts "n/a"
+end
+
