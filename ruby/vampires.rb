adbf81c820c4026135e648f58cbbe3e4a0c1f29a
tiffchiu <tiffxchiu@gmail.com>
2017-02-19 11:47:04 -0800

    Adds code to process multiple employees

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,36 +1,45 @@
-puts "What is your name?"
-name = gets.chomp
+puts "How many employees will be processed?"
+employees = gets.chomp.to_i
 
-puts "How old are you?"
-age = gets.chomp.to_i
+until employees == 0
 
-puts "What year were you born?"
-year_born = gets.chomp.to_i
+	puts "What is your name?"
+	name = gets.chomp
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you (y/n)?"
-garlic_bread = gets.chomp
+	puts "How old are you?"
+	age = gets.chomp.to_i
 
-puts "Would you like to enroll in the company's health insurance (y/n)?"
-insurance = gets.chomp
+	puts "What year were you born?"
+	year_born = gets.chomp.to_i
 
-real_age = 2017 - year_born
+	puts "Our company cafeteria serves garlic bread. Should we order some for you (y/n)?"
+	garlic_bread = gets.chomp
 
-if age == real_age
-	age_is_correct = true
-else
-	age_is_correct = false
+	puts "Would you like to enroll in the company's health insurance (y/n)?"
+	insurance = gets.chomp
+
+	real_age = 2017 - year_born
+
+	if age == real_age
+		age_is_correct = true
+	else
+		age_is_correct = false
+	end
+
+	if age_is_correct && (garlic_bread == "y" || insurance == "y")
+		result = "Probably not a vampire"
+	elsif !age_is_correct && garlic_bread == "n" && insurance == "n"		
+		result = "Almost certainly a vampire"
+	elsif !age_is_correct && (garlic_bread == "n" || insurance == "n")
+		result = "Probably a vampire"
+	elsif name == "Drake Cula" || name == "Tu Fang"
+		result = "Definitely a vampire"
+	else
+		result = "Results inconclusive"
+	end
+
+	puts result
+
+	employees = employees - 1
+
 end
-
-if age_is_correct && (garlic_bread == "y" || insurance == "y")
-	result = "Probably not a vampire"
-elsif !age_is_correct && garlic_bread == "n" && insurance == "n"		
-	result = "Almost certainly a vampire"
-elsif !age_is_correct && (garlic_bread == "n" || insurance == "n")
-	result = "Probably a vampire"
-elsif name == "Drake Cula" || name == "Tu Fang"
-	result = "Definitely a vampire"
-else
-	result = "Results inconclusive"
-end
-
-puts result
