f77c960ebd28af948a0e47d7f79439c7ff8e86d3
tiffchiu <tiffxchiu@gmail.com>
2017-02-19 10:55:04 -0800

    Adds program to ask employee to answer questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp.to_i
+
+puts "What year were you born?"
+year_born = gets.chomp.to_i
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you (y/n)?"
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance (y/n)?"
+insurance = gets.chomp
