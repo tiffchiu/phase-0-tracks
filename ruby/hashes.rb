ff125df36c8f0b88e63b52084ee013b8bfc2590b
tiffchiu <tiffxchiu@gmail.com>
2017-02-25 19:07:51 -0800

    Adds pseudocode for the interior design form

---
+++ ruby/hashes.rb
@@ -0,0 +1,16 @@
+# Pseudocode
+# - Ask designer/user to enter name, age, number of children, and decor theme
+# 	- For age, convert to integer
+# 	- For number of children, convert to integer
+# - Create hash with the variables
+# - Assign the input as a value to a hash
+# - Print the hash back on the screen
+# - Ask user if wants to change a value
+# 	If Yes
+# 	- User inputs name of key to change
+# 	- Ask user for new value of key
+# 	If No
+# 	- User inputs "none"
+# 	- Skip step
+# - Print newest version of hash
+# - Exit
