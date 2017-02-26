d52f154cc279e91a5d53be60cbfc3def57409c6d
tiffchiu <tiffxchiu@gmail.com>
2017-02-26 14:06:13 -0800

    Adds a researched method for sorting an array: bubble sort

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -32,3 +32,40 @@ p fib(6)
 p fib(13)
 p fib(100).last
 p fib(100).last == 218922995834555169026
+
+
+# Release 3: Sorting an Array
+
+# Bubble Sort
+# - Take an array and determine the length of it
+# - Create a loop that compares two items at a time
+# - If the item at the current position is greater than the one next to it, rearrange the order
+# - Add a word to end the loop
+
+def bubble_sort (array)
+
+  # Determining the length of the array (how many)
+  n = array.length
+  
+  loop do
+
+    # A variable to be maintained (so that it doesn't loop on infinitely)
+    swapped = false
+
+    (n-1).times do |i|
+
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        swapped = true
+      end
+    
+    end
+
+    break if not swapped
+  end
+
+  array
+end
+
+array = [42, 89, 23, 1]
+p bubble_sort (array)
