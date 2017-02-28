e9bdadc8ead74488b0bd14f65cf0731bf55acd1c
tiffchiu <tiffxchiu@gmail.com>
2017-02-27 20:22:47 -0800

    Designs a new Puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# SPECIES ------------------------
+# Puppius Maximus
+
+# CHARACTERISTICS ----------------
+# Energy level: varies
+# Hair length: varies
+# Eye count: 2
+# Paw count: 4
+# Color: varies
+
+# BEHAVIOR -----------------------
+# Run
+# Fetch
+# Go on walks
