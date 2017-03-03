4ec2d1eaaf27fc15319edfa8d49b332972a9c58a
tiffchiu <tiffxchiu@gmail.com>
2017-03-03 07:38:26 -0800

    Converts standalone module to mixin

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,38 @@
+# module Shout
+# 	def self.yell_angrily(words)
+#     	words + "!!!" + " :("
+#   	end
+
+#   	def self.yell_happily(words)
+# 		words + "!" + " :D"  	
+#   	end
+# end
+
+# p Shout.yell_angrily("What a day")
+# p Shout.yell_happily("What a day")
+
+# MIXIN
 module Shout
-	def self.yell_angrily(words)
-    	words + "!!!" + " :("
+	def yell_angrily(words)
+    	p words + "!!!" + " :("
   	end
 
-  	def self.yell_happily(words)
-		words + "!" + " :D"  	
+  	def yell_happily(words)
+		p words + "!" + " :D"  	
   	end
 end
 
-p Shout.yell_angrily("What a day")
-p Shout.yell_happily("What a day")
+class Coach
+	include Shout
+end
+
+class Cheerleader
+	include Shout
+end
+
+# DRIVER CODE
+coach = Coach.new
+coach.yell_angrily("That was not a foul")
+
+becky = Cheerleader.new
+becky.yell_happily("Let's go team")
