0c139b86722cfd430e26d1fac1cc74722c20dd74
tiffchiu <tiffxchiu@gmail.com>
2017-03-12 13:25:35 -0700

    Adds pseudocode for a reverse function

---
+++ js/explore.js
@@ -0,0 +1,9 @@
+// PSEUDOCODE
+// - Takes string as a parameter
+// - Breaks string down into individual characters
+// - Get a count of the number of letters in the word
+// - Loop through and take the last character of the word
+// - Add that last character to a new structure
+// - Keep looping until all characters are inside the structure
+// - Return that new word
+
